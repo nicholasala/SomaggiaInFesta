8ce0383180ac84df59dc5f6fc0a6ed0e57e14d00
Sala Nicholas <salanicholas.14@gmail.com>
2019-09-23 20:58:40 +0200

    Version 0.1

--- app/src/main/java/com/example/somaggiainfesta/network/KitchenFinder.java
+++ app/src/main/java/com/example/somaggiainfesta/network/KitchenFinder.java
@@ -21,23 +21,21 @@ public class KitchenFinder extends AsyncTask<Void, Void, Keys.kitchenState> {
 
     @Override
     protected Keys.kitchenState doInBackground(Void... voids) {
-        return Keys.kitchenState.FOUND;
+        try {
+            ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
 
-//        try {
-//            ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-//            NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-//
-//            if (activeNetwork == null || !activeNetwork.isConnectedOrConnecting())
-//                return Keys.kitchenState.NETERR;
-//
-//            if(InetAddress.getByAddress(Keys.ip.kitchen).isReachable(3000))
-//                return Keys.kitchenState.FOUND;
-//            else
-//                return Keys.kitchenState.NOTFOUND;
-//
-//        } catch (IOException e) {
-//            return Keys.kitchenState.NETERR;
-//        }
+            if (activeNetwork == null || !activeNetwork.isConnectedOrConnecting())
+                return Keys.kitchenState.NETERR;
+
+            if(InetAddress.getByAddress(Keys.ip.kitchen).isReachable(3000))
+                return Keys.kitchenState.FOUND;
+            else
+                return Keys.kitchenState.NOTFOUND;
+
+        } catch (IOException e) {
+            return Keys.kitchenState.NETERR;
+        }
     }
 
     @Override
